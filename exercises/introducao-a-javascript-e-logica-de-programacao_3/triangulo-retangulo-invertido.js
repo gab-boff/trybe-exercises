1733ae8d5ab9670fbd719cf951acbf20bdc9dd76
gab-boff <gmeninboff@gmail.com>
2021-04-06 13:57:38 -0300

    Adicionando Exercícios do Dia 5.1

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_3/triangulo-retangulo-invertido.js
@@ -0,0 +1,8 @@
+// n-1 espaços vazios
+// não se usa push, o array deve ser reescrito
+// spaces = asteriscos.push
+// começar pela última linha e fazer o console.log ao contrario
+
+// let n = 5;
+// let symbol = '*';
+// let inputLine = '';
